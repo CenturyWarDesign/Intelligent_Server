6d74dcf67af409a968877f430a516d3c5834ab00
wanbin <wanbin89731@163.com>
2013-10-31 22:23:14 +0800

    first java server

---
+++ src/centurywar/Main.java
@@ -0,0 +1,87 @@
+package centurywar;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class Main {
+	private int port = 8080;
+    private ServerSocket serverSocket;
+    private ExecutorService executorService;//线程池
+    private final int POOL_SIZE=10;//单个CPU线程池大小
+
+	public Main() throws IOException {
+        serverSocket=new ServerSocket(port);
+        //Runtime的availableProcessor()方法返回当前系统的CPU数目.
+        executorService=Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()*POOL_SIZE);
+        System.out.println("服务器启动");
+    }
+
+    public void service(){
+        while(true){
+            Socket socket=null;
+            try {
+                //接收客户连接,只要客户进行了连接,就会触发accept();从而建立连接
+                socket=serverSocket.accept();
+                executorService.execute(new Handler(socket));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+		new Main().service();
+    }
+
+}
+
+class Handler implements Runnable{
+    private Socket socket;
+    public Handler(Socket socket){
+        this.socket=socket;
+    }
+    private PrintWriter getWriter(Socket socket) throws IOException{
+        OutputStream socketOut=socket.getOutputStream();
+        return new PrintWriter(socketOut,true);
+    }
+    private BufferedReader getReader(Socket socket) throws IOException{
+        InputStream socketIn=socket.getInputStream();
+        return new BufferedReader(new InputStreamReader(socketIn));
+    }
+
+    public String echo(String msg){
+        return "echo:"+msg;
+    }
+    public void run(){
+        try {
+            System.out.println("New connection accepted "+socket.getInetAddress()+":"+socket.getPort());
+            BufferedReader br=getReader(socket);
+            PrintWriter pw=getWriter(socket);
+            String msg=null;
+            while((msg=br.readLine())!=null){
+                System.out.println(msg);
+                pw.println(echo(msg));
+                if(msg.equals("bye"))
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                if(socket!=null)
+                    socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
