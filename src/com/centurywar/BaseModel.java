ae726e981c2e5881ef64c074402a94e0c2cbbb78
wanbin <wanbin89731@163.com>
2013-11-11 23:42:05 +0800

    极光推送接口

--- src/com/centurywar/BaseModel.java
+++ src/com/centurywar/BaseModel.java
@@ -7,4 +7,16 @@ public class BaseModel {
 		Date date = new Date();
 		return (int) (date.getTime() / 1000);
 	}
+
+	/**
+	 * 发送推送消息到手机客户端
+	 * 
+	 * @param gameuid
+	 *            注意，这里写板子的id,里面会把所有手机的客户端都进行遍历的
+	 * @param content
+	 * @return
+	 */
+	public boolean sendToPush(int gameuid, String content) {
+		return true;
+	}
 }
