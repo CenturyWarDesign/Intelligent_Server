d044ec0b3b0a0cab79401aff8844a86223400e33
liuchunlong <huaxialiuchunlong@163.com>
2013-11-12 01:07:02 +0800

    更改触发手机推送温度

--- src/com/centurywar/User.java
+++ src/com/centurywar/User.java
@@ -3,7 +3,7 @@ package com.centurywar;
 import java.sql.ResultSet;
 
 public class User extends BaseModel {
-	public final static double LIMIT = 30;
+	public final static double LIMIT = 23;
 	private int gameuid = 0;
 	
 	private String secGameuid = "";
