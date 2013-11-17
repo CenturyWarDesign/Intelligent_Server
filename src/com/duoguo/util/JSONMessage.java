06c13b25092ba4d3a43146c6f5560950db2ae6f5
liuchunlong <huaxialiuchunlong@163.com>
2013-11-17 11:03:59 +0800

    json 字段util类

---
+++ src/com/duoguo/util/JSONMessage.java
@@ -0,0 +1,13 @@
+package com.duoguo.util;
+
+public class JSONMessage {
+
+	/** 功能码 */
+	public final static String CONTROL = "control";
+	/** 登陆用户名，返回验证码 */
+	public final static String USERNAME = "username";
+	/** 登陆密码串 */
+	public final static String PASSWORD = "paasword";
+//	public final static String RETCODE = "retCode";
+//	public final static String VERIFYCODE = "verifyCode";
+}
