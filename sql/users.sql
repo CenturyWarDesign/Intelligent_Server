733d235849698a5f35c81c9a8bf16b97ac603ce3
wanbin <wanbin89731@163.com>
2013-11-18 01:35:04 +0800

    users 添加一列

--- sql/users.sql
+++ sql/users.sql
@@ -6,3 +6,4 @@ CREATE  TABLE `intelligent`.`users` (
   `password` VARCHAR(100) NULL ,
   PRIMARY KEY (`id`) );
 alter table users add column client_id Int(11) default 0; 
+alter table users add column sec varchar(32) default "";
